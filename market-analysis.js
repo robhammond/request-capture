e3a10c57e54205c4d553895e278a92929cf6aa88
Rob Hammond <robhammond@hw030386.ad1.tm-gnet.com>
2017-10-05 15:21:29 +0100

    write to tsv

--- market-analysis.js
+++ market-analysis.js
@@ -6,7 +6,14 @@ const db = new sqlite3.Database('./domains.db', sqlite3.OPEN_READONLY, (err) => 
     if (err) {
         console.error(err.message);
     }
-    console.log('Connected to the database.');
+    // console.log('Connected to the database.');
+});
+const fs = require('fs');
+const tsv = 'scripts.tsv';
+
+fs.writeFile(tsv, "Source\tSub-Domain\tURL\tDomain\tName\n", (err) => {  
+    // throws an error, you could also catch it here
+    if (err) throw err;
 });
 
 // const urls = ['http://www.mirror.co.uk/', 
@@ -35,47 +42,59 @@ const coreDomain = reqUrl.match(/^https?:\/\/(?:www\.)?([^\/]+)/)[1];
 console.log("Fetching " + reqUrl + " - core domain: " + coreDomain);
 
 (async() => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.setRequestInterceptionEnabled(true);
     await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36');
     page.on('request', request => {
         let re1 = new RegExp('^https?://([^/]+)?' + coreDomain + '/.*?\.js.*$', 'i');
-        let re2 = new RegExp('^https?://([^/]+)?' + coreDomain + '/', 'i');
+        let ignoreRE = new RegExp('^https?://([^/]+)?' + coreDomain + '/', 'i');
 
         const url = request.url;
+        
+        let owner;
         let domain;
         let qDomain;
-        let owner;
-
-        if (!/^https?:\/\//i.test(request.url)) {
-            // do nothing (ie data:image)
-        } else if (re1.test(request.url)) {
+        if (url.match(/^https?:\/\//i)) {
             try {
                 domain = url.match(/^https?:\/\/([^\/]+)\//i)[1];
             } catch (err) {
-
+                console.log("ERROR: Can't match domain on " + url);
             }
-            console.log(coreDomain + "\t" + domain + "\t" + url);
-        } else if (re2.test(request.url)) {
-            // request for js resource
-            // console.log(request.url);
-        } else {
             try {
-                domain = url.match(/^https?:\/\/([^\/]+)\//i)[1];
-                let qDomain = domain.match(/([^.]+\.(?:com|co\.uk|net|io|org))$/)[1];
-                let sql = "SELECT domain, owner FROM domains WHERE domain LIKE '%" + qDomain + "%'";
-                
-                db.get(sql, [], (err, row) => {
-                    if (err) {
-                        throw err;
-                    }
-                    owner = parseResult(row);
-                    console.log(coreDomain + "\t" + domain + "\t" + url + "\t" + owner);
-                });
-
+                qDomain = domain.match(/([^.]+\.(?:com|co\.uk|net|io|org))$/)[1];
             } catch (err) {
+                console.log("ERROR: Can't match qDomain on " + url);
+            }
+            
 
+            if (!/^https?:\/\//i.test(request.url)) {
+                // do nothing (ie data:image)
+            } else if (re1.test(request.url)) { // internal JS files
+                console.log(coreDomain + "\t" + domain + "\t" + url + "\t" + qDomain);
+                let output = coreDomain + "\t" + domain + "\t" + url + "\t" + qDomain + "\n";
+                fs.appendFile(tsv, output, (err) => {  
+                    if (err) throw err;
+                });
+            } else if (ignoreRE.test(request.url)) { // ignore other internal requests
+            } else {
+                try {
+                    let sql = "SELECT domain, owner FROM domains WHERE domain LIKE '%" + qDomain + "%'";
+                    db.get(sql, [], (err, row) => {
+                        if (err) {
+                            throw err;
+                        }
+                        owner = parseResult(row);
+                        console.log(coreDomain + "\t" + domain + "\t" + url + "\t" + qDomain + "\t" + owner);
+                        let output = coreDomain + "\t" + domain + "\t" + url + "\t" + qDomain + "\t" + owner + "\n";
+                        fs.appendFile(tsv, output, (err) => {  
+                            if (err) throw err;
+                        });
+                    });
+
+                } catch (err) {
+
+                }
             }
         }
         request.continue();
