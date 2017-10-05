1c23f69406b7348a160b6b35f4b8367ac6e1ad5f
Rob Hammond <robhammond@hw030386.ad1.tm-gnet.com>
2017-10-05 14:47:24 +0100

    interacting with sqlite

--- market-analysis.js
+++ market-analysis.js
@@ -1,6 +1,13 @@
 'use strict';
 
 const puppeteer = require('puppeteer');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./domains.db', sqlite3.OPEN_READONLY, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the database.');
+});
 
 // const urls = ['http://www.mirror.co.uk/', 
 // 	''
@@ -9,6 +16,17 @@ const puppeteer = require('puppeteer');
 // const domainMap = {
 
 // };
+
+
+db.getAsync = function(query) {
+  return new Promise((resolve, reject) => this.get(query, function (err) {
+    if (err) {
+      reject(err)
+    } else {
+      resolve(this)
+    }
+  }))
+}
 
 
 const reqUrl = 'http://www.mirror.co.uk/';
@@ -22,19 +40,21 @@ console.log("Fetching " + reqUrl + " - core domain: " + coreDomain);
     await page.setRequestInterceptionEnabled(true);
     await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36');
     page.on('request', request => {
-    	let re1 = new RegExp('^https?://([^/]+)?' + coreDomain + '/.*?\.js.*$', 'i');
-    	let re2 = new RegExp('^https?://([^/]+)?' + coreDomain + '/', 'i');
+        let re1 = new RegExp('^https?://([^/]+)?' + coreDomain + '/.*?\.js.*$', 'i');
+        let re2 = new RegExp('^https?://([^/]+)?' + coreDomain + '/', 'i');
 
-    	const url = request.url;
-    	let domain;
+        const url = request.url;
+        let domain;
+        let qDomain;
+        let owner;
 
         if (!/^https?:\/\//i.test(request.url)) {
-        	// do nothing (ie data:image)
+            // do nothing (ie data:image)
         } else if (re1.test(request.url)) {
             try {
                 domain = url.match(/^https?:\/\/([^\/]+)\//i)[1];
             } catch (err) {
-                
+
             }
             console.log(coreDomain + "\t" + domain + "\t" + url);
         } else if (re2.test(request.url)) {
@@ -43,16 +63,28 @@ console.log("Fetching " + reqUrl + " - core domain: " + coreDomain);
         } else {
             try {
                 domain = url.match(/^https?:\/\/([^\/]+)\//i)[1];
-            } catch (err) {
+                let qDomain = domain.match(/([^.]+\.(?:com|co\.uk|net|io|org))$/)[1];
+                let sql = "SELECT domain, owner FROM domains WHERE domain LIKE '%" + qDomain + "%'";
                 
+                db.get(sql, [], (err, row) => {
+                    if (err) {
+                        throw err;
+                    }
+                    owner = printRow(row);
+                    console.log(coreDomain + "\t" + domain + "\t" + url + "\t" + owner);
+                });
+
+            } catch (err) {
+
             }
-            console.log(coreDomain + "\t" + domain + "\t" + url);
         }
         request.continue();
     });
     await page.goto(reqUrl, { waitUntil: 'networkidle' });
-    await page.screenshot({path: 'news.png', fullPage: true});
+    await page.screenshot({ path: 'news.png', fullPage: true });
 
 
     await browser.close();
 })();
+
+
