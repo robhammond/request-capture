b53d3d3d5b0b3ef6dad48e78c79582cdbecc2913
Rob Hammond <robhammond@hw030386.ad1.tm-gnet.com>
2017-10-05 11:14:56 +0100

    more details

--- market-analysis.js
+++ market-analysis.js
@@ -1,20 +1,58 @@
+'use strict';
+
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setRequestInterceptionEnabled(true);
-  page.on('request', request => {
-  if (/^https?:\/\/([^\/]+)?mirror\.co\.uk\//i.test(request.url)) {
-	    // request for js resource
-	    // console.log(request.url);
-	  } else {
-	  	console.log(request.url);
-	  }
-	  request.continue();
-	});
-  await page.goto('http://www.mirror.co.uk/', {waitUntil: 'networkidle'});
-  
+// const urls = ['http://www.mirror.co.uk/', 
+// 	''
+// ];
 
-  await browser.close();
+// const domainMap = {
+
+// };
+
+
+const reqUrl = 'http://www.mirror.co.uk/';
+const coreDomain = reqUrl.match(/^https?:\/\/(?:www\.)?([^\/]+)/)[1];
+
+console.log("Fetching " + reqUrl + " - core domain: " + coreDomain);
+
+(async() => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.setRequestInterceptionEnabled(true);
+    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36');
+    page.on('request', request => {
+    	let re1 = new RegExp('^https?://([^/]+)?' + coreDomain + '/.*?\.js.*$', 'i');
+    	let re2 = new RegExp('^https?://([^/]+)?' + coreDomain + '/', 'i');
+
+    	const url = request.url;
+    	let domain;
+
+        if (!/^https?:\/\//i.test(request.url)) {
+        	// do nothing (ie data:image)
+        } else if (re1.test(request.url)) {
+            try {
+                domain = url.match(/^https?:\/\/([^\/]+)\//i)[1];
+            } catch (err) {
+                
+            }
+            console.log(coreDomain + "\t" + domain + "\t" + url);
+        } else if (re2.test(request.url)) {
+            // request for js resource
+            // console.log(request.url);
+        } else {
+            try {
+                domain = url.match(/^https?:\/\/([^\/]+)\//i)[1];
+            } catch (err) {
+                
+            }
+            console.log(coreDomain + "\t" + domain + "\t" + url);
+        }
+        request.continue();
+    });
+    await page.goto(reqUrl, { waitUntil: 'networkidle' });
+    await page.screenshot({path: 'news.png', fullPage: true});
+
+
+    await browser.close();
 })();
